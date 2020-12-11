09a1106a0ba729653f0a1a60cb4f9c81295c3398
bearbearbear <bear-bear-bear@kakao.com>
2020-12-11 23:08:28 +0900

    feat: js 파일들을 모듈화

---
+++ src/js/initUniverse.js
@@ -0,0 +1,44 @@
+class Star {
+  constructor(isShootingStar) {
+    this.star = document.createElement('div');
+    this.star.classList.add('star');
+
+    this.star.style.left = `${Math.random() * 100}%`;
+    this.star.style.top = `${Math.random() * 100}%`;
+
+    if (isShootingStar) this.toShootingStar();
+
+    return this.star;
+  }
+
+  toShootingStar() {
+    this.star.classList.add('shootingStar');
+    this.star.style.animationDelay = `${Math.random() * 10}s`;
+  }
+}
+
+const createStars = (num, isShootingStar = false) => {
+  let stars = [];
+  const _Star = Star;
+
+  for (let i = 0; i < num; i++) {
+    stars.push(new _Star(isShootingStar));
+  }
+
+  return stars;
+};
+
+const initUniverse = () => {
+  const universe = document.querySelector('.universe');
+
+  const fixedStars = createStars(50);
+  const shootingStars = createStars(50, true);
+
+  const DOMfragement = document.createDocumentFragment();
+
+  DOMfragement.append(...fixedStars, ...shootingStars);
+
+  universe.append(DOMfragement);
+};
+
+export default initUniverse;
