b194c12dcf2b4631d3d1afaaac644f9d9fd95081
bearbearbear <bear-bear-bear@kakao.com>
2020-12-13 15:23:38 +0900

    feat: 우주선 엘리먼트를 생성할 js파일 생성, 호출

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import 'regenerator-runtime/runtime';
 import initUniverse from './initUniverse';
 import initGreeting from './initGreeting';
+import launchSpacecraft from './launchSpacecraft';
 
 const init = () => {
   initUniverse();
   initGreeting();
+  launchSpacecraft();
 };
 
 document.addEventListener('DOMContentLoaded', init, false);
