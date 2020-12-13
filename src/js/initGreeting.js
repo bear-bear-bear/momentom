6aa1c2c586f62e3c192dfa5039ee08f2e7cea1fe
bearbearbear <bear-bear-bear@kakao.com>
2020-12-14 05:12:24 +0900

    feat: greeting 을 비동기처리

--- src/js/initGreeting.js
+++ src/js/initGreeting.js
@@ -1,31 +1,34 @@
-const greetingForm = document.querySelector('.greeting');
-const localStorage = window.localStorage;
-
-const setHideGreeting = () => {
-  const greetingNode = document.createElement('p');
-
-  greetingNode.textContent = `안녕하세요 ${localStorage.getItem('userName')}님`;
-  greetingNode.classList.add('hide');
-  greetingNode.classList.add('greeting');
-
-  greetingForm.parentNode.replaceChild(greetingNode, greetingForm);
-};
-
-const handlegreetingForm = (e) => {
-  e.preventDefault();
-
-  const userName = greetingForm.querySelector('.greeting__input').value || 'Annonymous';
-
-  localStorage.setItem('userName', userName);
-
-  setHideGreeting();
-};
-
 const initGreeting = () => {
-  const userName = localStorage.getItem('userName');
+  const greetingForm = document.querySelector('.greeting');
+  const localStorage = window.localStorage;
+  return new Promise((resolve, reject) => {
+    const setHideGreeting = () => {
+      const greetingNode = document.createElement('p');
 
-  if (userName) setHideGreeting();
-  else greetingForm.addEventListener('submit', handlegreetingForm, false);
+      greetingNode.textContent = `안녕하세요 ${localStorage.getItem('userName')}님`;
+      greetingNode.classList.add('hide');
+      greetingNode.classList.add('greeting');
+
+      greetingForm.parentNode.replaceChild(greetingNode, greetingForm);
+
+      resolve();
+    };
+
+    const handlegreetingForm = (e) => {
+      e.preventDefault();
+      greetingForm.removeEventListener('submit', handlegreetingForm);
+
+      const name = greetingForm.querySelector('.greeting__input').value || 'Annonymous';
+      localStorage.setItem('userName', name);
+
+      setHideGreeting();
+    };
+
+    const userName = localStorage.getItem('userName');
+
+    if (userName) setHideGreeting();
+    else greetingForm.addEventListener('submit', handlegreetingForm, false);
+  });
 };
 
 export default initGreeting;
