e1fd70d8ca5dfc4bd3d5a8798084afa9ce4b90e6
bearbearbear <bear-bear-bear@kakao.com>
2020-12-16 04:04:32 +0900

    refacotr: greet.js의 프로미스 구문을 async로 변경

--- src/js/greet.js
+++ src/js/greet.js
@@ -1,45 +1,45 @@
-const greet = () => {
-  return new Promise((resolve, reject) => {
-    const greeting = document.querySelector('.greeting');
+const greet = async () => {
+  const greeting = document.querySelector('.greeting');
 
-    const greetDown = () => {
-      greeting.style.opacity = '0';
-      greeting.style.top = '52%';
-    };
+  const greetDown = () => {
+    greeting.style.opacity = '0';
+    greeting.style.top = '52%';
+  };
 
-    const greetUp = () => {
-      greeting.style.opacity = '1';
-      greeting.style.top = '50%';
-    };
+  const greetUp = () => {
+    greeting.style.opacity = '1';
+    greeting.style.top = '50%';
+  };
 
-    const replaceMsg = (str) => {
-      greeting.textContent = str;
-    };
+  const replaceMsg = (str) => {
+    greeting.textContent = str;
+  };
 
-    greetDown();
+  const textMove = (moveFunc, ms = 800) => {
+    return new Promise((resolve) => {
+      moveFunc();
+      setTimeout(resolve, ms);
+    });
+  };
+  const stop = (ms = 800) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    setTimeout(() => {
-      greetUp();
-      replaceMsg(`안녕하세요 ${localStorage.getItem('userName')}님`);
-    }, 800);
+  await textMove(greetDown);
 
-    setTimeout(() => {
-      greetDown();
-    }, 2400);
+  replaceMsg(`안녕하세요 ${localStorage.getItem('userName')}님`);
 
-    setTimeout(() => {
-      greetUp();
-      replaceMsg(`여행을 시작합니다.`);
-    }, 3200);
+  await textMove(greetUp);
+  await stop();
+  await textMove(greetDown);
 
-    setTimeout(() => {
-      greetDown();
-    }, 4800);
+  replaceMsg(`여행을 시작합니다.`);
 
-    setTimeout(() => {
-      resolve();
-    }, 6400);
-  });
+  await textMove(greetUp);
+  await stop();
+  await textMove(greetDown);
+
+  await stop();
+
+  return;
 };
 
 export default greet;
