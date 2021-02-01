d52f1f2103cf3a0d55ad10663ad574a7663b7769
bearbearbear <bear-bear-bear@kakao.com>
2021-02-02 02:31:51 +0900

    feat: 시계 기능 구현

---
+++ src/js/setData/_setTimeData.js
@@ -0,0 +1,30 @@
+const hours = document.querySelector('.clock__time__hours');
+const minutes = document.querySelector('.clock__time__minutes');
+const AM_PM = document.querySelector('.clock__time__AM-PM');
+
+const setTimeData = () => {
+  const now = new Date();
+  let h = now.getHours(); // 0 - 23
+  let m = now.getMinutes(); // 0 - 59
+  let session = 'AM';
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = 'PM';
+  }
+
+  h = h < 10 ? '0' + h : h; // 암시적 타입변환 num -> string
+  m = m < 10 ? '0' + m : m; // 암시적 타입변환 num -> string
+
+  hours.textContent = h;
+  minutes.textContent = m;
+  AM_PM.textContent = session;
+
+  setTimeout(setTimeData, 1000); // 재귀 실행
+};
+
+export default setTimeData;
--- src/js/setData/setData.js
+++ src/js/setData/setData.js
@@ -1,7 +1,9 @@
 import setWeatherData from './_setWeatherData';
+import setTimeData from './_setTimeData';
 
 const setData = () => {
   setWeatherData();
+  setTimeData();
 };
 
 export default setData;
